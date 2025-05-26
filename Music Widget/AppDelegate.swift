6e9dcd036aa2c3b0ff25adccfe2cb292410fa258
Corvin Gröning <114770097+cgroening@users.noreply.github.com>
2025-05-26 13:29:39 +0200

    Update AppDelegate.swift

--- Music Widget/AppDelegate.swift
+++ Music Widget/AppDelegate.swift
@@ -7,36 +7,34 @@
 
 import SwiftUI
 
+
 class AppDelegate: NSObject, NSApplicationDelegate {
-    // Instance of Music Model
+    /// Shared instance of the MusicModel
     let musicModel: MusicModel = .shared
     
-    /// Indicates whether the app should always be displayed "Always On Top"
+    /// AppStorage variable to store the "Always On Top" state
     @AppStorage("alwaysOnTopDisabled") var alwaysOnTopDisabled = false
     
     /// Creates an observer for "com.apple.Music.playerInfo" and sets
-    /// `window.level = .floating` for all windows for "Always On Top"
+    /// `window.level = .floating` for all windows if the
+    /// "Always On Top" setting is disabled.
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Observer
         let dnc = DistributedNotificationCenter.default()
         dnc.addObserver(self, selector: #selector(AppDelegate.updateState),
                         name: NSNotification.Name(
-                            rawValue: "com.apple.Music.playerInfo"),
-                        //                            rawValue: "com.apple.Music.library.read-write"),
-                        //                            rawValue: "com.apple.Music.library.sourceSaved"),
-                        //                            rawValue: "com.apple.Music.playback"),
-                        //                        object: nil)
+                            rawValue: "com.apple.Music.playerInfo"
+                        ),
                         object: "com.apple.Music.player")
-        //                        object: "com.apple.Music.library")
         
-        // Always On Top?
+        // Set the window level to floating if "Always On Top" is disabled
         for window in NSApplication.shared.windows {
             window.level = alwaysOnTopDisabled ? .normal : .floating
         }
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Nothing happens here yet.
+        // Nothing happens here, yet.
     }
     
     /// Updates the Music Model after the Music app triggers the signal
@@ -45,8 +43,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Is a track playing?
         if let message = aNotification.userInfo as NSDictionary?,
            message["Name"] as? String == nil {
-            // No, no music is playing, or the Music app is being closed.
-            // Wait a moment to avoid AppleScript errors.
+            // No music is playing or the Music app is being closed,
+            // wait a second to avoid AppleScript errors
             sleep(1)
         }
         Task {
